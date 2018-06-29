a2ccd0b7b1326eb97f2b0f09d9a12792521d533d
palagpatski <palagpatski@gmail.com>
2018-06-29 18:24:03 +0800

    Update maintenance page header

---
+++ maintenance-page.tpl.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @file
+ * Fusion theme implementation to display a single Drupal page while offline.
+ *
+ * All the available variables are mirrored in page.tpl.php. Some may be left
+ * blank but they are provided for consistency.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_maintenance_page()
+ */
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+
+<body class="<?php print $classes; ?>">
+  <div id="page" class="page">
+    <div id="page-inner" class="page-inner">
+
+      <!-- header-group region: width = grid_width -->
+      <div id="header-group-wrapper" class="header-group-wrapper full-width clearfix">
+        <div id="header-group" class="header-group region <?php print $grid_width; ?>">
+          <div id="header-group-inner" class="header-group-inner inner clearfix">
+            <?php print render($page['header']); ?>
+          </div><!-- /header-group-inner -->
+        </div><!-- /header-group -->
+      </div><!-- /header-group-wrapper -->
+
+      <!-- main region: width = grid_width -->
+      <div id="main-wrapper" class="main-wrapper full-width">
+        <div id="main" class="main region <?php print $grid_width; ?>">
+          <div id="main-inner" class="main-inner inner">
+            <div id="content-region" class="content-region region nested">
+              <div id="content-region-inner" class="content-region-inner inner">
+                <a id="main-content-area"></a>
+                <div id="content-inner" class="content-inner block">
+                  <div id="content-inner-inner" class="content-inner-inner inner">
+                    <?php if ($title): ?>
+                    <h1 class="title"><?php print $title; ?></h1>
+                    <?php endif; ?>
+                    <?php if ($content): ?>
+                    <div id="content-content" class="content-content">
+                      <?php print $content; ?>
+                    </div><!-- /content-content -->
+                    <?php endif; ?>
+                  </div><!-- /content-inner-inner -->
+                </div><!-- /content-inner -->
+              </div><!-- /content-region-inner -->
+            </div><!-- /content-region -->
+          </div><!-- /main-inner -->
+        </div><!-- /main -->
+      </div><!-- /main-wrapper -->
+
+    </div><!-- /page-inner -->
+  </div><!-- /page -->
+</body>
+</html>
